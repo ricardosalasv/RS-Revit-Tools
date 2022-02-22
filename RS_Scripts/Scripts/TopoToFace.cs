7a3cb01c21b91e554119948758733ff74504bb68
ricardosalasv <ricardo.salas.cg@gmail.com>
2022-02-22 01:38:56 -0300

    feat: evaluating points of faces edges

--- RS_Scripts/Scripts/TopoToFace.cs
+++ RS_Scripts/Scripts/TopoToFace.cs
@@ -29,7 +29,24 @@ namespace RS_Scripts.Scripts
             {
                 IList<Reference> selectedFaces = sel.PickObjects(ObjectType.Face, "Please select the face(s)");
 
-                IList<Face> faces = selectedFaces.Select(x => doc.GetElement(x).GetGeometryObjectFromReference(x) as Face).ToList();
+                IList<PlanarFace> faces = selectedFaces.Select(x => doc.GetElement(x).GetGeometryObjectFromReference(x) as PlanarFace).ToList();
+                IList<PlanarFace> nonVerticalFaces = faces.Select(x => x).Where(x => x.FaceNormal.Z != 0).ToList();
+
+                IList<XYZ> newPoints = new List<XYZ>();
+
+                foreach (var face in nonVerticalFaces)
+                {
+                    foreach (CurveLoop curveLoop in face.GetEdgesAsCurveLoops())
+                    {
+                        foreach (Curve curve in curveLoop)
+                        {
+                            for (double i = 0.01; i <= 0.99; i = i + 0.05)
+                            {
+                                newPoints.Add(curve.Evaluate(i, true));
+                            }
+                        }
+                    }
+                }
 
                 return selectedFaces;
             }
